5a0fd28cf915b0c6f17c41034ad29bd938475552
Smile259 <smile02006@outlook.com>
2025-12-23 08:03:24 +0530

    Add lecture 2

---
+++ Lecture02.py
@@ -0,0 +1,22 @@
+# Lecture second of python
+print("Hey I am a good girl/boy \nThis viewer is a also a good girl/boy") # Escape Sequence Character
+
+print("Hey I am\"Student\"\nand I am studying Python Language")
+
+#This is a single line comment
+print("This is a print statement")
+print("Hello World!!!")  # Printing hello world
+
+'''It will execute a block of code if a specified condition is true
+If the condition is false then it will execute another block of code'''    # Multiline Comments
+
+a = 9
+if (a > 4):
+    print("a is greater then 4")
+else:
+     print("a is not greater than 4")
+
+# More on print statement
+
+print("Hey",5,8, sep="~", end="009\n")
+print("Unknown")
