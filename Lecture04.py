6288148e5ec6697fb09e5e74b6a8e12e958bb332
Smile259 <smile02006@outlook.com>
2025-12-24 08:46:23 +0530

    Add lecture 4

---
+++ Lecture04.py
@@ -0,0 +1,30 @@
+# Operators
+a = 3
+b = 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+
+# Calculator
+x = 15
+y = 2
+
+add = x+y
+sub = x-y
+mul = x*y
+div = x/y
+mod = x%y
+floordiv = x//y
+pow = x**y
+
+print("Addition:",add)
+print("Substraction:",sub)
+print("Multiplication:",mul)
+print("Division:",div)
+print("Modulus:",mod)
+print("Floor Division:",floordiv)
+print("Exponential:",pow)
