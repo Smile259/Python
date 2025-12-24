59ab36fcc07c7ebc4eb1f5fce8a982d984211e0a
Smile259 <smile02006@outlook.com>
2025-12-24 08:00:36 +0530

    Add lecture 3

---
+++ Lecture03.py
@@ -0,0 +1,30 @@
+# Variables And Data Types
+a = 863637683163
+print(a)
+print(type(a))
+b = "Unknown"
+print(b)
+print(type(b))
+c = True
+print(True)
+print(type(c))
+d = None
+print(None)
+print(type(d))
+x = 100
+y = 66
+print(x+y)
+
+# List
+list1 = [[2,3],232,979,["Apple","Mango"]]
+print(list1)
+
+# Tuple
+tuple1 = ((2,3),232,979,("Apple","Mango"))
+print(tuple1)
+
+# Dict
+dict1 = {"name":"Sakshi","age":"20","canVote":"True"}
+print(dict1)
+
+
