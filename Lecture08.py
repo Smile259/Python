e98f5569a437b70cd09adba449cb4728860ef8bd
Smile259 <smile02006@outlook.com>
2025-12-26 23:37:00 +0530

    Add lecture 8

---
+++ Lecture08.py
@@ -0,0 +1,14 @@
+# Strings Slicing and Operations on String
+
+fruit = "Mango"
+mangoLen = len(fruit)
+print(mangoLen)    # Length of a string
+print(fruit[0:4])
+print(fruit[:4])   # String as an Array
+print(fruit[:])
+print(fruit[:-3])  #print(fruit[:len(fruit)-3])
+print(fruit[-1:-3]) #print(fruit[-1:len(fruit)-3])
+print(fruit[-3:-1]) 
+
+nm = "Harry"
+print(nm[-4:-2])
