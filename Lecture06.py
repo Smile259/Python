7e62e2bac03044ae66d688a08befca8e9c970605
Smile259 <smile02006@outlook.com>
2025-12-26 21:37:10 +0530

    Add lecture 6

---
+++ Lecture06.py
@@ -0,0 +1,21 @@
+# Taking User Input
+
+a = input("Enter your name: ")
+print("My name is: ",a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+print(x+y)
+
+print(int(x)+int(y))
+print(int(x)-int(y))
+print(int(x)*int(y))
+print(int(x)/int(y))
+print(int(x)//int(y))
+print(int(x)%int(y))
+print(int(x)**int(y))
+
+
+m = int(input("Enter the first number: "))
+n = float(input("Enter the second number: "))
+print(m+n)
