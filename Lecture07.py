7b035a8f3b5783eee19fb00994d566c7403f8348
Smile259 <smile02006@outlook.com>
2025-12-26 22:35:23 +0530

    Add lecture 7

---
+++ Lecture07.py
@@ -0,0 +1,23 @@
+# Strings
+
+name = "Priya"
+friend = "Smile"
+anotherFriend = "Mitali"
+apple = "He said,\"I want to eat an apple" # apple = 'He said,\"I want to eat an apple'
+cherry = '''She said,
+Hi Harry
+hey I am good
+"I want to eat cherry'''
+print("Hello, " + name)
+print(apple)
+print(cherry)
+
+# Accessing Characters through the string
+print(name[0])  # Positive index
+print(name[-1]) # Negative index
+
+# Looping through the string
+print("Lets use a for loop\n")
+for character in apple:
+    print(character)
+
