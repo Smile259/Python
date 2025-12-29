b3b365930158dfbaae4da8a1baf905180428d2bd
Smile259 <smile02006@outlook.com>
2025-12-29 15:34:30 +0530

    Add lecture 14

---
+++ Lecture14.py
@@ -0,0 +1,24 @@
+# While Loop
+
+i = 0 
+while(i<3):
+    print(i)
+    i=i+1
+
+count = 5
+while(count > 0):
+    print(count)
+    count = count-1
+
+x = int(input("Enter the number:"))
+while(x<=38):
+    print(x)
+    x=x+1
+
+# Else with while loop
+y = 6
+while(y>0):
+    print(y)
+    y = y-1
+else:
+    print("I am inside else!!!")
