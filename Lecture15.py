481f81e0e484c47ddf26016041c1c0a4ac2cfc1b
Smile259 <smile02006@outlook.com>
2026-01-10 11:14:02 +0530

    Add lecture 15

---
+++ Lecture15.py
@@ -0,0 +1,15 @@
+# Break statement
+for i in range(12):
+    if(i == 10):
+        break
+    print("5 X",i+1,"=",5 * (i+1))
+
+print("Break out of the loop")
+
+# Continue Statement
+for i in range(12):
+    if(i == 10):
+        print("skip the iteration")
+        continue
+    print("5 X",i,"=",5 * i)
+       
