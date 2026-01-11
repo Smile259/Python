45c5b64ec248bc89200ac1958b7f909fbb2ce3b7
Smile259 <smile02006@outlook.com>
2026-01-11 23:14:44 +0530

    Add lecture 16

---
+++ Lecture16.py
@@ -0,0 +1,29 @@
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+        print("First number is greater")
+    else:
+        print("Second number is greater")
+
+def islesser(a,b):
+    pass
+
+a = 9
+b =8
+#gmean1 = (a*b)/(a+b)
+#print(gmean1)
+isGreater(a,b)
+
+calculateGmean(a,b)
+
+
+c = 8
+d = 10
+#gmean2 = (c*d)/(c+d)
+#print(gmean2)
+isGreater(c,d)
+
+calculateGmean(c,d)
